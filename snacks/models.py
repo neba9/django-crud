862a63ac604a8db36ad4a0f8e4aeb8d7865675a7
nebiyu kifle <rema9.ne@gmail.com>
2021-03-06 22:00:14 -0800

    first commit

---
+++ snacks/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+# Create your models here.
+class snack(models.Model):
+  title = models.CharField(max_length=64)
+  purchaser = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+  description = models.TextField(default='')
+
+  def __str__(self):
+    return self.title
+
+  def __str__(self):
+    return reverse('snack_detail', args=[str(self.id)])
+
+
+  
---
+++ snacks/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import snack
+from django.urls import reverse_lazy
+
+# Create your views here.
+class SnackListView(ListView):
+  template_name = 'snack_list.html'
+  model = snack
+
+
+class SnackDetailView(DeleteView):
+  template_name = 'snack_detail.html'
+  model = snack
+
+
+class SnackCreateView(CreateView):
+  template_name = 'snack_create.html'
+  model = snack
+  fields = ['title', 'purchaser', 'description']
+
+
+class SnackUpdateView(UpdateView):
+  template_name = 'snack_update.html'
+  model = snack
+  fields = ['title', 'purchaser', 'description']
+
+
+class SnackDeleteView(DeleteView):
+  template_name = 'snack_delete.html'
+  model = snack
+  success_url = reverse_lazy('snack_list')
